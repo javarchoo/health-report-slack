e6a85e0ad006eb1544f153f3fd86cd5d2d6b5b47
javarchoo <jawon.choo@gmail.com>
2018-08-14 04:02:24 +0000

    init

---
+++ get_ceph_health.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Set Env
+WORK_DIR=/root/ceph-health
+DATA_DIR=$WORK_DIR/data
+
+# Get Ceph Status
+for HOSTS in cicd dev stg prd
+do ssh ceph-$HOSTS 'ceph -s' > $DATA_DIR/ceph-$HOSTS
+done
+
+# Get Status list
+grep health -H -r $DATA_DIR | awk -F/ '{print $NF}' | awk '{print $1 "\t" $3}' > $WORK_DIR/cephstatuslist
+#cat $WORK_DIR/cephstatuslist
+
+python $WORK_DIR/post.py $1 $2
---
+++ post.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+"""Post slack message."""
+
+# https://github.com/os/slacker
+# https://api.slack.com/methods
+
+import os
+import sys
+from slacker import Slacker
+
+
+def post_slack():
+    """Post slack message."""
+    try:
+
+        token=sys.argv[2]
+        slack = Slacker(token)
+
+        work_dir="/root/ceph-health"
+        data_dir="/root/ceph-health/data"
+        body = "#" * 30 + "\n# Ceph Health Check Detail\n" + "#" * 30 + "\n"
+        files = ["cicd", "dev", "stg", "prd"]
+        for f in files:
+            f_name = data_dir + "/ceph-" + f
+            tf = open(f_name,"r")
+            body+="ceph-" + f + "\n"
+            body+="```\n"
+            body+=tf.read()
+            body+="```\n\n"
+
+        body += "#" * 30 + "\n# Ceph Health Check Simple\n" + "#" * 30 + "\n"
+        body+="```\n"
+        f_name = work_dir + "/cephstatuslist"
+        body += open(f_name, "r").read()
+        body+="```"
+
+        channel = "#" + sys.argv[1]
+        obj = slack.chat.post_message(channel, body)
+#        print(obj.successful, obj.__dict__['body']['channel'], obj.__dict__[
+#            'body']['ts'])
+    except KeyError as ex:
+        print('Environment variable %s not set.' % str(ex))
+
+if __name__ == '__main__':
+    post_slack()
