6f2884087952e3579df6af96c9b3d45431cbc71f
Jawon Choo <jawon.choo@gmail.com>
2018-08-27 18:26:56 +0900

    report detail only ERROR/NG cases

--- post.py
+++ post.py
@@ -18,15 +18,23 @@ def post_slack():
 
         work_dir="/root/ceph-health"
         data_dir="/root/ceph-health/data"
+
+
+        dic = {}
+        lines = open("/root/ceph-health/cephstatuslist", "r").readlines()
+        for line in lines:
+            dic[line.split('\t')[0].replace(":", "")] = line.split('\t')[1].replace("\n","")
+
         body = "#" * 30 + "\n# Ceph Health Check Detail\n" + "#" * 30 + "\n"
         files = ["cicd", "dev", "stg", "prd"]
         for f in files:
-            f_name = data_dir + "/ceph-" + f
-            tf = open(f_name,"r")
-            body+="ceph-" + f + "\n"
-            body+="```\n"
-            body+=tf.read()
-            body+="```\n\n"
+            if 'HEALTH_OK' != dic["ceph-"+f]:
+                f_name = data_dir + "/ceph-" + f
+                tf = open(f_name,"r")
+                body+="ceph-" + f + "\n"
+                body+="```\n"
+                body+=tf.read()
+                body+="```\n\n"
 
         body += "#" * 30 + "\n# Ceph Health Check Simple\n" + "#" * 30 + "\n"
         body+="```\n"
